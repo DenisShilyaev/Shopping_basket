b0ae1e3c82a20f00b96b25cbaf28c67c071741a9
unknown <79194954002@yandex.ru>
2019-11-20 22:37:26 +0500

    3 commit

--- js/serviceProducts.js
+++ js/serviceProducts.js
@@ -16,15 +16,33 @@ class ServiceProducts {
 
         for (var i = 0; i < this.productsCatalog.length; i++) {
 
-            var item = document.createElement('div');
-            item.setAttribute('class', 'item');
+            var item = this.getElement({ tagName: 'div', className: 'item' });
+            var name = this.getElement({ tagName: 'div', className: 'name', innerText: this.productsCatalog[i].name });
+            var img = this.getElement({ tagName: 'div', className: 'img', backgroundImage: `url(${this.productsCatalog[i].img})` });
+            var price = this.getElement({ tagName: 'div', className: 'price', innerText: this.productsCatalog[i].price.toLocaleString() + ' USD' });
+            var btn = this.getElement({ tagName: 'button', className: 'btn', innerText: 'Добавить в корзину' });
+
+            item.appendChild(name);
+            item.appendChild(img);
+            item.appendChild(price);
+            item.appendChild(btn);
             wrapper.appendChild(item);
         }
         this.container.appendChild(wrapper);
     }
 
     getElement(options) {
-        //
+        var element = document.createElement(options.tagName);
+        if ('className' in options) {
+            element.setAttribute('class', options.className);
+        }
+        if ('innerText' in options) {
+            element.innerText = options.innerText;
+        }
+        if ('backgroundImage' in options) {
+            element.style.backgroundImage = options.backgroundImage;
+        }
+        return element;
     }
 
     actions() {
