e3201c06c903ee27f6ca4f2e494c946b9e176c93
unknown <79194954002@yandex.ru>
2019-11-21 14:39:53 +0500

    4 commit

--- js/serviceProducts.js
+++ js/serviceProducts.js
@@ -4,31 +4,33 @@ class ServiceProducts {
         this.productsCatalog = productsCatalog;
         this.create();
 
-        /*        <div class="item">
+        /*		Так должны выглядеть итоговые элемент, создаваемый с помощью методов create и getElement:
+        		<div class="item">
                     <div class="name">CANON</div>
                     <div class="img" style="background-image: url(https://media.istockphoto.com/photos/cup-of-tea-on-a-delicate-pink-background-with-pink-meringues-picture-id1127396690);"></div>
                     <div class="price">1000</div>
                     <button class="btn">Добавить в корзину</button>
                 </div>*/
     }
-    create() {
-        var wrapper = document.createElement('slot');
+    create() {//Создаем необходимые элементы
+        var wrapper = document.createElement('slot');//Обертка. Необходима, что бы каждый раз не производить манипуляции с DOM, а выводить в DOM все элементы из каталога разом
 
         for (var i = 0; i < this.productsCatalog.length; i++) {
-
+        	//Создаем необходимые элементы с помощью метода getElement:
             var item = this.getElement({ tagName: 'div', className: 'item' });
             var name = this.getElement({ tagName: 'div', className: 'name', innerText: this.productsCatalog[i].name });
             var img = this.getElement({ tagName: 'div', className: 'img', backgroundImage: `url(${this.productsCatalog[i].img})` });
             var price = this.getElement({ tagName: 'div', className: 'price', innerText: this.productsCatalog[i].price.toLocaleString() + ' USD' });
             var btn = this.getElement({ tagName: 'button', className: 'btn', innerText: 'Добавить в корзину' });
 
+            //Добавляем элементы в обертку:
             item.appendChild(name);
             item.appendChild(img);
             item.appendChild(price);
             item.appendChild(btn);
             wrapper.appendChild(item);
         }
-        this.container.appendChild(wrapper);
+        this.container.appendChild(wrapper);//Добавляем оертку в DOM
     }
 
     getElement(options) {
@@ -50,4 +52,4 @@ class ServiceProducts {
     }
 }
 
-var serviceProducts = new ServiceProducts('.container-products', productsCatalog);
+var serviceProducts = new ServiceProducts('.container-products', productsCatalog);//Создаем экземпляр класса
